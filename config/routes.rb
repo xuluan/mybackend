d5568fe87d2994d6d55812059cc5b97eb3cda073
xuluan <rolle.xu@gmail.com>
2013-08-27 20:57:35 +0800

    creat tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Mybackend::Application.routes.draw do
+
+  namespace :api do
+    namespace :v1  do
+      resources :tokens,:only => [:create, :destroy]
+    end
+  end
+
   devise_for :users
   root :to => "magazines#index"
   resources :magazines
